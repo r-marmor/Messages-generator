2619ce60e120ad6cc830a1b3a9ad73eccee5ce17
106Manner <102478451+106Manner@users.noreply.github.com>
2022-07-24 17:25:14 +0100

    push in an array the caracteristics of the object

--- app.js
+++ app.js
@@ -5,5 +5,30 @@ const randomNumber = (num) => {
 const randomGift = {
     type: ['Pokemon', 'Pen', 'Power Ranger', 'Bag'],
     color: ['Red', 'Gold', 'Purple', 'Blue', 'White', 'Black'],
-    pureness: ['45%', '55%', '65%', '100%']
+    pureness: ['75%', '85%', '95%', '100%']
+};
+
+const yourGift = [];
+
+for (caract in randomGift) {
+    
+    let rdmIndex = randomNumber(randomGift[caract].length);
+
+    switch(prop) {
+        
+        case 'type':
+            yourGift.push(`Your random gift is a : ${randomGift[caract][rdmIndex].}`)
+            break;
+        
+        case 'color':
+            yourGift.push(`Which color is : ${randomGift[caract][rdmIndex]}`)
+            break;
+
+        case 'pureness':
+            yourGift.push(`Its pureness is : ${randomGift[caract][rdmIndex]}`)
+            break;
+
+        default: yourGift.push("Error: can't identify your gift")
+    
+    }
 };
