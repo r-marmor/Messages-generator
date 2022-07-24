76e4465336a52d1f803057eeb3320568489ec5cc
106Manner <102478451+106Manner@users.noreply.github.com>
2022-07-24 17:08:58 +0100

    create a randomgift

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const randomNumber = (num) => {
     Math.floor(Math.random) * num;
 }
 
+const randomGift = {
+    type: ['Pokemon', 'Pen', 'Power Ranger', 'Bag'],
+    color: ['Red', 'Gold', 'Purple', 'Blue', 'White', 'Black'],
+    pureness: ['45%', '55%', '65%', '100%']
+};
